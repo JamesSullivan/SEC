8c3b0b44418e9c63882b97283997cb595d637996
James Sullivan <james.brian.sullivan@gmail.com>
2025-05-09 10:30:01 -0600

    Refactor fin_report class to improve database connection and query logic; update company name retrieval from users to distinct names from sub.

--- tables/FS_Notes_Query.py
+++ tables/FS_Notes_Query.py
@@ -3,19 +3,24 @@
 import os
 import pandas as pd
 import psycopg
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, CursorResult
+from typing import List
 
 class fin_report():
                         
     def __init__(self):
-        password = os.environ.get('AKASAKA_DB_PW', 'default_password')
+        password: str = os.environ.get('AKASAKA_DB_PW', 'default_password')
         # Set up the connection to PostgreSQL
-        self.engine = create_engine(f'postgresql://puser:{password}@localhost:5432/finreport') 
-
+        self.engine = create_engine(f'postgresql://puser:{password}@localhost:5432/fin_report') 
+        self.companies: List[str] = []
         with self.engine.connect() as connection:
-            result = connection.execute(text("select username from users"))
+            result: CursorResult = connection.execute(text("select distinct(name) from sub;"))
             for row in result:
-                print("username:", row.username)
+                self.companies.append(row.name)
 
 
 
+fr = fin_report()
+print(fr.companies)
+
+# %%
