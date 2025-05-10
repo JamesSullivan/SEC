b6a63c85473571f85776205a5074e6a0b3544b80
James Sullivan <james.brian.sullivan@gmail.com>
2025-05-09 23:48:26 -0600

    Refactor fin_report class to include company CIK retrieval and update company list query

--- tables/FS_Notes_Query.py
+++ tables/FS_Notes_Query.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import psycopg
 from sqlalchemy import create_engine, text, CursorResult
-from typing import List
+from typing import List, Dict, Any
 
 class fin_report():
                         
@@ -13,14 +13,16 @@ class fin_report():
         # Set up the connection to PostgreSQL
         self.engine = create_engine(f'postgresql://puser:{password}@localhost:5432/fin_report') 
         self.companies: List[str] = []
+        self.company_cik: Dict[str, int] = {}
         with self.engine.connect() as connection:
-            result: CursorResult = connection.execute(text("select distinct(name) from sub;"))
+            result: CursorResult = connection.execute(text("select distinct(name), cik from sub;"))
             for row in result:
                 self.companies.append(row.name)
+                self.company_cik[row.name] = row.cik
 
 
 
 fr = fin_report()
-print(fr.companies)
+print(fr.company_cik)
 
 # %%
